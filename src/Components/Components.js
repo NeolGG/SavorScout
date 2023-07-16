969584c26d639aee80306b69f91233c1baffb193
NeolGG <neolgutierrez@gmail.com>
2023-07-16 16:13:57 -0400

    added routing

---
+++ src/Components/Components.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+//pages
+import Friends from "./Friends/Friends";
+import RestaurantRec from "./RestaurantRec/RestaurantRec";
+import Dashboard from "./Dashboard/Dashboard";
+
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+
+import AuthModule from "./Auth/Auth";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
+
+export default function Components() {
+    return (
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<ProtectedRoute path="/" element={Dashboard} />}
+          /> 
+  
+          <Route
+            path="/friends"
+            element={<ProtectedRoute path="/friends" element={Friends} />}
+          />
+          <Route
+            path="/communities"
+            element={<ProtectedRoute path="/restuarantrecs" element={RestaurantRec} />}
+          />
+  
+          <Route path="/auth" element={<AuthModule />} /> 
+          <Route path="/auth/register" element={<AuthRegister />} />
+          <Route path="/auth/login" element={<AuthLogin />} />
+  
+  
+          <Route path="*" element={<Navigate to="/auth" replace />} />
+        </Routes>
+      </Router>
+    )
+  }
