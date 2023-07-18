b634db47bfaf0bdb3379855a2289d6cb4bdb6efc
NeolGG <neolgutierrez@gmail.com>
2023-07-17 21:14:59 -0400

    bug fixes

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Communities from '../Communities/Communities.js'
 
 export default function Dashboard() {
-    return (
-    <html>
-        <Communities/>
-    </html>
-    )
+  return (
+    <div>Dashboard</div>
+  )
 }
