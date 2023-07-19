458653d88a04987fde085289bd075e7a3b5c9cf4
NeolGG <neolgutierrez@gmail.com>
2023-07-18 20:32:45 -0400

    ratings

--- src/common/services/RatingsService.js
+++ src/common/services/RatingsService.js
@@ -6,8 +6,8 @@ Ratings.collection = [];
 export const getAllRatings = () => {
     const Rating = Parse.Object.extend("Rating");
     const query = new Parse.Query(Rating);
-    query.include("Restaurant");
-    query.include("User");
+    query.include("Restaurant"); //allows pointer to be read
+    query.include("User"); // allows pointer to be read
     return query.find().then((results) => {
         return results;
     });
