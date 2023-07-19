a61444dd86d7fa77db1fcc96eeab6b5c877454d0
NeolGG <neolgutierrez@gmail.com>
2023-07-19 15:48:10 -0400

    added dashboard

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { checkUser } from "../../common/services/AuthService";
 
 const ProtectedRoute = ({ element: Component, ...rest }) => {
   console.log("element: ", Component);
-  const navigate = useNavigate();
 
   // Check if the user is authenticated
   if (checkUser()) {
