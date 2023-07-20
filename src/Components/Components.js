9182f7b9260e9f22f1f431270e1e036ccd8420dd
NeolGG <neolgutierrez@gmail.com>
2023-07-20 01:21:16 -0400

    bug fixes

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,58 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import Dashboard from './Dashboard/Dashboard';
-import NewRatings from './NewRatings/NewRatings';
-import Verified from './Verified/Verified';
+import React from "react";
 
-import AuthModule from './Auth/Auth';
-import AuthRegister from './Auth/AuthRegister';
-import AuthLogin from './Auth/AuthLogin';
-import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+//pages
+import Friends from "./Friends/Friends";
+import RestaurantRec from "./RestaurantRec/RestaurantRec";
+import Dashboard from "./Dashboard/Dashboard";
+import NewRatings from "./NewRatings/NewRatings";
+import Verified from "./Verified/Verified";
+import VerRatings from "./VerRatings/VerRatings";
+
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+
+import AuthModule from "./Auth/Auth";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 export default function Components() {
-<<<<<<< HEAD
     return (
       <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<ProtectedRoute path="/" element={Dashboard} />}
-          /> 
-  
-          <Route
-            path="/friends"
-            element={<ProtectedRoute path="/friends" element={Friends} />}
-          />
-          <Route
-            path="/restaurantrecs"
-            element={<ProtectedRoute path="/restuarantrecs" element={RestaurantRec} />}
-          />
-          <Route
-            path="/newratings"
-            element={<ProtectedRoute path="/newratings" element={NewRatings} />}
-          />
-          <Route
-            path="/verified"
-            element={<ProtectedRoute path="/verified" element={Verified} />}
-          />
-          <Route
-            path="/verifiedratings"
-            element={<ProtectedRoute path="/verifiedratings" element={VerRatings} />}
-          />
-  
-          <Route path="/auth" element={<AuthModule />} /> 
-          <Route path="/auth/register" element={<AuthRegister />} />
-          <Route path="/auth/login" element={<AuthLogin />} />
-  
-  
-          <Route path="*" element={<Navigate to="/auth" replace />} />
-        </Routes>
-      </Router>
-    )
-  }
-=======
-  return (
-    <Router>
       <Routes>
         <Route path="/" element={<ProtectedRoute path="/" component={Dashboard} />} />
         <Route path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
@@ -64,6 +29,42 @@ export default function Components() {
         <Route path="*" element={<Navigate to="/auth" replace />} />
       </Routes>
     </Router>
-  );
-}
->>>>>>> origin/main
+
+  //     <Router>
+  //       <Routes>
+  //         <Route
+  //           path="/"
+  //           element={<ProtectedRoute path="/" element={Dashboard} />}
+  //         /> 
+  
+  //         <Route
+  //           path="/friends"
+  //           element={<ProtectedRoute path="/friends" element={Friends} />}
+  //         />
+  //         <Route
+  //           path="/restaurantrecs"
+  //           element={<ProtectedRoute path="/restuarantrecs" element={RestaurantRec} />}
+  //         />
+  //         <Route
+  //           path="/newratings"
+  //           element={<ProtectedRoute path="/newratings" element={NewRatings} />}
+  //         />
+  //         <Route
+  //           path="/verified"
+  //           element={<ProtectedRoute path="/verified" element={Verified} />}
+  //         />
+  //         <Route
+  //           path="/verifiedratings"
+  //           element={<ProtectedRoute path="/verifiedratings" element={VerRatings} />}
+  //         />
+  
+  //         <Route path="/auth" element={<AuthModule />} /> 
+  //         <Route path="/auth/register" element={<AuthRegister />} />
+  //         <Route path="/auth/login" element={<AuthLogin />} />
+  
+  
+  //         <Route path="*" element={<Navigate to="/auth" replace />} />
+  //       </Routes>
+  //     </Router>
+    )
+  }
