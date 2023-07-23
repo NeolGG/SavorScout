89e848264a28b8701548ad8ee3de45a43d322e4e
emayor4 <emayor02@gmail.com>
2023-07-22 22:26:57 -0500

    verified ratings

--- src/Components/VerRatings/VerRatings.js
+++ src/Components/VerRatings/VerRatings.js
@@ -1,31 +1,31 @@
+import './VerRatings.css';
 import React, {useState,useEffect} from 'react'
 import { RatingsCont, getAllVerifiedRatings } from '../../common/services/RatingsService'
-import VerRatingsList from './VerRatingsList';
+import VerRatingsList from './VerRatingsList'
 
 const VerRatings = () => {
   const [vRatings, setvRatings] = useState([]);
 
-    useEffect(() => {
-      if (RatingsCont.collection.length) {
-        setvRatings(RatingsCont.collection);
-      } else {
-        getAllVerifiedRatings().then((vRatings) => {
-          setvRatings(vRatings);
-        });
-      }
-    }, []);
-    
-    return (
-      <div>
-          <hr />
-          These are the Verified ratings
-          <hr />
-          <div>
-            <VerRatingsList vRatings = {vRatings}/>
-          </div>
-      </div>
-    );
+  useEffect(() => {
+    if (RatingsCont.collection.length) {
+      setvRatings(RatingsCont.collection);
+    } else {
+      getAllVerifiedRatings().then((vRatings) => {
+        setvRatings(vRatings);
+      });
+    }
+  }, []);
 
+  return (
+    <div className="verratings-container">
+      <hr className="verratings-divider"/>
+      <div className="verratings-title">★ Verified Ratings ★</div>
+      <hr className="verratings-divider"/>
+      <div>
+        <VerRatingsList vRatings={vRatings} />
+      </div>
+    </div>
+  );
 }
 
-export default VerRatings
+export default VerRatings;
--- src/Components/VerRatings/VerRatingsList.js
+++ src/Components/VerRatings/VerRatingsList.js
@@ -1,23 +1,26 @@
-const VerRatingsList = ({vRatings}) => {
-    return (
-      <div>
-        <div>
-          <p> </p>
-          {vRatings.length > 0 && (
-            <ul>
-              {vRatings.map((vRating) => (
-                <li key={"1" + vRating.id}> 
-                  {" "}
-                  {vRating.get("User").get("firstName")} {vRating.get("User").get("lastName")} <br />
-                  {vRating.get("Restaurant").get("Name")}<br />
-                  {vRating.get("Rating")} Stars
-                </li> 
-              ))}
-            </ul>
-          )}
-        </div>{" "}
-      </div>
-    );
+import React from 'react';
+import './VerRatings.css'; 
+
+const VerRatingsList = ({ vRatings }) => {
+  return (
+    <div className="verratings-container">
+      <div className="verratings-grid">
+        {vRatings.length > 0 && vRatings.map((vRating) => (
+          <div className="verratings-card" key={"1" + vRating.id}>
+            <div className="verratings-info">
+              {vRating.get("User").get("firstName")} {vRating.get("User").get("lastName")} rated
+              <i>{vRating.get("Restaurant").get("Name")}</i>
+              for
+              <b>{vRating.get("Rating")} Stars</b>
+              <div className="rating-stars">
+                {'★'.repeat(vRating.get("Rating"))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>{" "}
+    </div>
+  );
 }
 
-export default VerRatingsList
+export default VerRatingsList;
