65a594f8873b72a7dcb6df88ab76fb0956735de1
NeolGG <neolgutierrez@gmail.com>
2023-07-22 22:14:38 -0400

    bug fixes

--- src/common/services/RestaurantService.js
+++ src/common/services/RestaurantService.js
@@ -10,6 +10,26 @@ export const getAllRestaurants = () => { // exports queries for filtering in "re
     return query;
 };
 
+export const getAllVerifiedRatings = () => {
+    const Rating = Parse.Object.extend("Rating"); 
+    const query = new Parse.Query(Rating); //Ratings Query
+    query.include("Restaurant");
+    query.include("User");
+    query.find().then((results)=>{
+        console.log("reuslts",results);
+    })
+
+    const User = Parse.Object.extend("User"); // makes new object with the user class
+    const userQuery = new Parse.Query(User); // gets query of users 
+    userQuery.equalTo("Verified", true); // filters out only verified users
+    
+    query.matchesQuery("User", userQuery); // Add this line to filter by the "Verified" attribute of the User class 
+
+    return query.find().then((results) => {
+        return results;
+    });
+}
+
 export const getCuisinebyName = (name) => {
     const Cuisine = Parse.Object.extend("Restaurant");
     const query = new Parse.Query(Cuisine)
